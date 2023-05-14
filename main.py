cf8863896e11cb71c256cecfa9d724549a148e31
S.M Hassan <118466320+Muhammad-Hassan12@users.noreply.github.com>
2023-05-15 00:26:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ def hash_string(string):
     hash_object = hashlib.sha256(string.encode('utf-8'))
     return hash_object.hexdigest()
 
-# Generate a large string
 string = "abcdefghijklmnopqrstuvwxyz" * 100000
 
 start_time = time.time()
 
-# Hash the string repeatedly in a loop
 while True:
     hash_string(string)
 
-    # Check if the desired time has elapsed
     elapsed_time = time.time() - start_time
-    if elapsed_time >= 60:  # Change the time limit as needed
+    if elapsed_time >= 60:
         break
